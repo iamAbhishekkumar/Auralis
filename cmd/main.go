daa69f83788554c66e3b9e0a96c5a9f3eeecf07e
iamAbhishekkumar <abhisonuk123@gmail.com>
2025-03-19 21:04:00 +0530

    created simple server

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,20 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"net"
+	"os"
+)
 
+func main() {
+	l, err := net.Listen("tcp", "0.0.0.0:6379")
+	if err != nil {
+		fmt.Fprint(os.Stderr, "Failed to bind to port 6379")
+		os.Exit(1)
+	}
+	_, err = l.Accept()
+	if err != nil {
+		fmt.Println("Error accepting connection: ", err.Error())
+		os.Exit(1)
+	}
 }
